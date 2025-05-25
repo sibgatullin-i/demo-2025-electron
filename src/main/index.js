4945d5921e897e2b107cf2271f3d680c6429941f
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2025-05-25 17:10:15 +0300

    working

--- src/main/index.js
+++ src/main/index.js
@@ -5,12 +5,45 @@ import icon from '../../resources/icon.png?asset'
 
 import connectDB from './db';
 
-async function foo(event, data) {
+async function getPartners() {
   try {
-    console.log(data)
-    dialog.showMessageBox({ message: 'message back' })
+    const response = await global.dbclient.query(`SELECT T1.*,
+    CASE WHEN sum(T2.production_quantity) > 300000 THEN 15
+    WHEN sum(T2.production_quantity) > 50000 THEN 10
+    WHEN sum(T2.production_quantity) > 10000 THEN 5
+    ELSE 0 
+    END as discount
+    from partners as T1
+    LEFT JOIN sales as T2 on T1.id = T2.partner_id
+    GROUP BY T1.id`)
+    return response.rows
   } catch (e) {
-    dialog.showErrorBox('Ошибка', e)
+    console.log(e)
+  }
+}
+async function createPartner(event, partner) {
+  const { type, name, ceo, email, phone, address, rating } = partner;
+
+  try {
+    await global.dbclient.query(`INSERT into partners (organization_type, name, ceo, email, phone, address, rating) values('${type}', '${name}', '${ceo}', '${email}', '${phone}', '${address}', ${rating})`)
+    dialog.showMessageBox({ message: 'Успех! Партнер создан' })
+  } catch (e) {
+    console.log(e)
+    dialog.showErrorBox('Ошибка', "Партнер с таким именем уже есть")
+  }
+}
+async function updatePartner(event, partner) {
+  const { id, type, name, ceo, email, phone, address, rating } = partner;
+
+  try {
+    await global.dbclient.query(`UPDATE partners
+      SET name = '${name}', organization_type = '${type}', ceo='${ceo}', email='${email}', phone='${phone}', address='${address}', rating='${rating}'
+      WHERE partners.id = ${id};`)
+    dialog.showMessageBox({ message: 'Успех! Данные обновлены' })
+    return;
+  } catch (e) {
+    dialog.showErrorBox('Невозможно создать пользователя', 'Такой пользователь уже есть')
+    return ('error')
   }
 }
 
@@ -19,6 +52,7 @@ function createWindow() {
     width: 900,
     height: 670,
     show: false,
+    icon: join(__dirname, '../../resources/icon.ico'),
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
@@ -48,7 +82,9 @@ app.whenReady().then(async () => {
 
   global.dbclient = await connectDB();
 
-  ipcMain.handle('sendSignal', foo)
+  ipcMain.handle('getPartners', getPartners)
+  ipcMain.handle('createPartner', createPartner)
+  ipcMain.handle('updatePartner', updatePartner)
 
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,19 +1,49 @@
-import { useEffect } from 'react'
-import electronLogo from './assets/electron.svg'
+import { useEffect, useState } from "react"
+import { Link } from "react-router";
+import { useNavigate } from "react-router-dom";
+import logo from './assets/logo.png'
 
 function App() {
+  const navigate = useNavigate();
+  const [partners, setPartners] = useState([]);
   useEffect(() => {
-    (async (data="test") => await window.api.foo(data))()
+    (async () => {
+      const res = await window.api.getPartners()
+      setPartners(res)
+    })()
   }, [])
-
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <h1>Hello, world!</h1>
+      <div className="page-heading">
+        <img className="page-logo" src={logo} alt="" />
+        <h1>Партнеры</h1>
+      </div>
+      <ul className="partners-list">
+        {partners.map((partner) => {
+          return <li className="partner-card" key={partner.id} onClick={() => { navigate('/update', { state: { partner } }) }}>
+            <div className="partner-data">
+              <p className="card_heading">{partner.organization_type} | {partner.name}</p>
+              <div className="partner-data-info">
+                <p>{partner.ceo}</p>
+                <p>{partner.phone}</p>
+                <p>Рейтинг: {partner.rating}</p>
+              </div>
+            </div>
+            <div className="partner-sale partner-data card_heading">
+              {partner.discount}%
+            </div>
+          </li>
+        })}
+      </ul>
+
+      <Link to={'/create'}>
+        <button>
+          Создать партнера
+        </button>
+      </Link>
     </>
   )
 }
 
 export default App
-
